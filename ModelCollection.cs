88cd281d114b7c9b2c3e6b7f17e1ec6e9a562c07
Granting Technology - GT (Community) <30596361+GrantingTechnology@users.noreply.github.com>
2019-10-04 15:22:45 +0200

    Adjust and inclusion new feature - ModelCollectio
    Making it possible to remove items from a collection, object native GT Framework 2.0

---
+++ ModelCollection.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace gt.business.container
+{
+    public class ModelCollection
+    {
+        #region Attribute
+        private int _count = 0;
+        private IList<string> _objectname = new List<string>();
+        public IDictionary<Type, object> models = new Dictionary<Type, object>();
+        #endregion
+
+        #region Property
+        public int Count { get { return _count; } }
+        public IList<string> collectionObjectName { get { return _objectname; } }
+        #endregion
+
+        /// <summary>
+        /// Storage of used collections
+        /// </summary>
+        private void listObject()
+        {
+            foreach (var keyValue in models)
+                collectionObjectName.Add(keyValue.ToString().ToLower());
+        }
+
+        /// <summary>
+        /// Remove item collection type
+        /// </summary>
+        /// <param name="index">Object Type: Dynamic </param>
+        public void RemoveItem(dynamic index) {
+
+            models.Remove(index.GetType());
+
+        }
+
+        /// <summary>
+        /// Method that adds objects to the collection, one type per collection
+        /// </summary>
+        /// <typeparam name="T">Object Type: Anonymous, type reference to be added</typeparam>
+        /// <param name="t">Objetct Type: Predicate</param>
+        public void AddModel<T>(T t)
+        {
+            models.Add(t.GetType(), t);
+            _count++;
+            listObject();
+        }
+
+        /// <summary>
+        /// Method to rescue the collection, inside the container there can be many objects
+        /// </summary>
+        /// <typeparam name="T">Object Type: Anonymous, type reference to be added</typeparam>
+        /// <param name="t">Objetct Type: Predicate</param>
+        public T GetModel<T>()
+        {
+            try
+            {
+                return (T)models[typeof(T)];
+            }
+            catch (Exception e)
+            {
+                throw new System.ArgumentException("Fail GetModel", e);
+            }
+
+        }
+    }
+}
